7e36021bb2ecca1c9ce8867ec9355cbbdd15e543
Puvel <puvelko@gmail.com>
2020-04-02 16:10:48 +0300

    ver01

--- src/changeTheme.js
+++ src/changeTheme.js
@@ -1,3 +1,4 @@
+import { loadStorage, saveStorage } from './storage';
 const refs = {
   themeCheckbox: document.querySelector('.js-switch-input'),
   body: document.body,
@@ -8,7 +9,7 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-const darkTheme = localStorage.getItem('Theme');
+const darkTheme = loadStorage('Theme');
 
 if (darkTheme === 'DARK') {
   refs.themeCheckbox.checked = true;
@@ -21,18 +22,10 @@ function hendleChange(e) {
   if (e.currentTarget.checked === true) {
     refs.body.classList.remove(Theme.LIGHT);
     refs.body.classList.add(Theme.DARK);
-    try {
-      localStorage.setItem('Theme', 'DARK');
-    } catch (err) {
-      console.error('Set state error: ', err);
-    }
+    saveStorage('Theme', 'DARK');
   } else if (e.currentTarget.checked === false) {
     refs.body.classList.remove(Theme.DARK);
     refs.body.classList.add(Theme.LIGHT);
-    try {
-      localStorage.setItem('Theme', 'LIGHT');
-    } catch (err) {
-      console.error('Set state error: ', err);
-    }
+    saveStorage('Theme', 'LIGHT');
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-import menu from './menu.json';
-import listItemTemplate from './templates/listItemTemplate.hbs';
 import changeThem from './changeTheme';
+import addMarkup from './addMarkup';
 import './styles.css';
-
-const refs = {
-  menuList: document.querySelector('.js-menu'),
-};
-
-function buildMarkup(menu) {
-  const createMarkup = menu
-    .map(menuItem => listItemTemplate(menuItem))
-    .join('');
-  refs.menuList.insertAdjacentHTML('beforeend', createMarkup);
-}
-
-buildMarkup(menu);
-
